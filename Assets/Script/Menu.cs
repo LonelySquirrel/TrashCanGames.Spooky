cda71dbbfc550b3657319a27b58fa1a70d3ef56e
TCG-MrFrog <linus.barger44@gmail.com>
2021-02-25 18:41:58 +0100

    Made a menu
    I made a start menu with option, start and quit

---
+++ Assets/Script/Menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    //These all simply load certain scenes when you press a button
+    public void StartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void back()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
