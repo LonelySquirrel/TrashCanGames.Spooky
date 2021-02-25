d0a7d28bf3114d269945cc9844347b2e66ca795b
LonelySquirrel <79454326+LonelySquirrel@users.noreply.github.com>
2021-02-25 17:00:04 +0100

    shit not working

---
+++ Assets/Script/Inventory.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField]
+    Animator Opens;
+    [SerializeField]
+    Animator Closes;
+    public bool isopen = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isopen == false)
+        {
+            if(Input.GetKeyDown(KeyCode.Tab))
+            {
+                Open();
+                isopen = true;
+            }
+        }
+        else if (isopen == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                Close();
+                isopen = false;
+            }
+            
+        }
+    }
+
+    void Open()
+    {
+        print("Opening");
+        Opens.SetTrigger("Open");
+    }
+    void Close()
+    {
+        print("Closeing");
+        Closes.SetTrigger("Close");
+    }
+}
+
