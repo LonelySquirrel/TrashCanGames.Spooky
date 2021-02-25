3990908cfe3f5576e7f0bb326a1d8b8b74eb0f11
Christian-Baza <79457485+Christian-Baza@users.noreply.github.com>
2021-02-25 19:30:00 +0100

    inventory

---
+++ Assets/Script/Item.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item : MonoBehaviour
+{
+    Inventory inventory;
+    [SerializeField]
+    string itemname;
+    [SerializeField]
+    Sprite itemSprite;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>(); //Hittar tar "Inventory" komponenten från spelaren
+    }
+
+    private void OnMouseDown()
+    {
+        for (int i = 0; i < inventory.slots.Length; i++) //Kollar alla slots för en tom ruta
+        {
+            if (inventory.slotsUsed[i] == false) 
+            {
+                inventory.slotsUsed[i] = true; // Gör att rutan är full
+                inventory.slots[i].name = itemname; // Ger namnet till objektet
+                inventory.slots[i].GetComponent<Image>().sprite = itemSprite; // Ger Spriten till objektet
+                inventory.slots[i].GetComponent<Image>().enabled = true; // Visar Spriten -Christian
+                break;
+            }
+        }
+    }
+
+
+}
