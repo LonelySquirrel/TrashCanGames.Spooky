fb018511d35a5ca5f646d0605c3cff3d13415999
lnfera <79453730+lnfera@users.noreply.github.com>
2021-02-26 11:25:36 +0100

    WIP: Overlay to conversations

---
+++ Assets/Script/Dialog/DialogManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+    public void StartDialogue(Dialog dialogue) 
+    {
+        Debug.Log("Starting conversation with " + dialogue.name);
+        sentences.Clear();
+
+        foreach (string sentance in dialogue.sentence) 
+        {
+            sentences.Enqueue(sentance);
+        }
+        DisplayNextSentance();
+            
+    }
+    public void DisplayNextSentance() 
+    { 
+        if(sentences.Count == 0 )
+        {   
+            EndDialogue();
+            return;
+        }
+        string sentances = sentences.Dequeue();
+        Debug.Log(sentances);
+    }
+    void EndDialogue() 
+    {
+        Debug.Log("The conversation ends here...");
+    }
+}
