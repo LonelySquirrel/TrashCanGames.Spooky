da6a0a9284e5d980e486b64017d3f13595ac92fe
LonelySquirrel <79454326+LonelySquirrel@users.noreply.github.com>
2021-03-08 13:14:31 +0100

    FadeIN and Out

---
+++ Assets/Script/RoomChange.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomChange : MonoBehaviour
+{
+    public Image FadeO;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        FadeO.canvasRenderer.SetAlpha(1.0f);
+        FadeIN();
+        
+    }
+
+    // Update is called once per frame
+  
+    void FadeIN()
+    {
+        FadeO.CrossFadeAlpha(0, 1, false);
+    }
+    public void FadeOut()
+    {
+        FadeO.CrossFadeAlpha(1, 1, false);
+    }
+}
