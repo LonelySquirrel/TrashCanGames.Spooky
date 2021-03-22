a7f00f7d26b6591632b13a93104c75d023968831
Emil Lehtola <79453730+lnfera@users.noreply.github.com>
2021-03-22 14:48:41 +0100

    Finished the dialog script except for the continue button that doesn't work for some reason

--- Assets/Script/Dialog/DialogManager.cs
+++ Assets/Script/Dialog/DialogManager.cs
@@ -1,17 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class DialogManager : MonoBehaviour
 {
     private Queue<string> sentences; // Skapar en Queue för X antal meningar.
-
+    public TMP_Text text;
+    public Image textBox;
+    public Button continueButton;
+    private bool textActive;
     void Start()
     {
         sentences = new Queue<string>();    //Där du väljer nya strings till Queue.
+        textActive = false;
+        continueButton.enabled = false;
+    }
+    private void Update()
+    {
+        if(textActive == false) 
+        { text.text = " ";
+            textBox.enabled = false;
+            continueButton.enabled = false;
+        }
+        else { textBox.enabled = true;
+            continueButton.enabled = true;  
+              }
     }
     public void StartDialogue(Dialog dialogue) 
     {
+
         Debug.Log("Starting conversation with " + dialogue.name); // Tar "name" från dialog och gör den till en convesations inledare.
         sentences.Clear(); // rensar dom gammla meningarna och gör plats för nästa i Queue.
 
@@ -27,10 +46,19 @@ public class DialogManager : MonoBehaviour
         if(sentences.Count == 0 ) // om antalet meningar i "Queue" är == 0 så avslutas dialogen. 
         {   
             EndDialogue();
+            textActive = false;
+            textBox.enabled = false;
+            continueButton.enabled = false;
             return;
         }
+        else 
+        {
+            textActive = true;
+        }
+        
         string sentances = sentences.Dequeue(); // Tar bort den mening som ligger längst fram och ersätter den men näst kommande.
         Debug.Log(sentances); // skriver ut den nya 1an på Queue
+        text.text = sentances;
     }
     void EndDialogue() 
     {
