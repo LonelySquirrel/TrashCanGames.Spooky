91bb3b97b054639fb131c8de4620d22f2401c83f
TCG-MrFrog <linus.barger44@gmail.com>
2021-04-01 16:19:33 +0200

    Gjorde en credit scene
    Jag skapade credits scenen och skapade några saker ska fin slipa kodden senare

---
+++ Assets/Script/Endscene.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Endscene : MonoBehaviour
+{
+    public GameObject Hjalmar;
+    public GameObject Cristian;
+    public GameObject Emil;
+    public GameObject Linus;
+
+    public GameObject Kaylei;
+    public GameObject Madelene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Hjalmar.SetActive(false);
+        Cristian.SetActive(false);
+        Emil.SetActive(false);
+        Linus.SetActive(false);
+        Kaylei.SetActive(false);
+        Madelene.SetActive(false);
+    }
+
+    public void hjalmar()
+    {
+        Hjalmar.SetActive(false);
+        Cristian.SetActive(false);
+        Emil.SetActive(false);
+        Linus.SetActive(false);
+        Kaylei.SetActive(false);
+        Madelene.SetActive(false);
+    }
+    public void cristian()
+    {
+        Hjalmar.SetActive(false);
+        Cristian.SetActive(false);
+        Emil.SetActive(false);
+        Linus.SetActive(false);
+        Kaylei.SetActive(false);
+        Madelene.SetActive(false);
+    }
+    public void emil()
+    {
+        Hjalmar.SetActive(false);
+        Cristian.SetActive(false);
+        Emil.SetActive(false);
+        Linus.SetActive(false);
+        Kaylei.SetActive(false);
+        Madelene.SetActive(false);
+    }
+    public void linus()
+    {
+        Hjalmar.SetActive(false);
+        Cristian.SetActive(false);
+        Emil.SetActive(false);
+        Linus.SetActive(false);
+        Kaylei.SetActive(false);
+        Madelene.SetActive(false);
+    }
+    public void graf1()
+    {
+        Hjalmar.SetActive(false);
+        Cristian.SetActive(false);
+        Emil.SetActive(false);
+        Linus.SetActive(false);
+        Kaylei.SetActive(false);
+        Madelene.SetActive(false);
+    }
+    public void graf2()
+    {
+        Hjalmar.SetActive(false);
+        Cristian.SetActive(false);
+        Emil.SetActive(false);
+        Linus.SetActive(false);
+        Kaylei.SetActive(false);
+        Madelene.SetActive(false);
+    }
+}
